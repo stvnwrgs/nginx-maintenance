77969fc2c40a607ac5dea48b5b053f432d8f6183
Steven Wirges <steven.wirges@antevorte.org>
2015-01-10 13:52:30 +0100

    added ssl options

--- installer.sh
+++ installer.sh
@@ -10,16 +10,32 @@
 #
 
 PATH_TO_WWW="/var/www/maintenance"
-SEARCH="{{servernames}}"
+SERVERNAMES="{{servernames}}"
+SSL_CERT=""
+SSL_KEY=""
 
-if [ "$#" -ne 1 ]; then
+echo "$#"
+
+if [ "$#" -lt 1 ]; then
     echo "please give me some servernames"
-    echo "example: installer.sh 'server.com servername2.com'"
+    echo "example: installer.sh 'server.com servername2.com' /path/to/sslcert /path/to/sslkey"
     exit 1
 fi
 echo "$1"
 
-if [ ! -d "$CLIENT_BUILD_DIR" ]
+if [ "$#" -eq 3 ]; then
+    echo "WITH SSL"
+
+    if [ ! -f "$2" ] || [ ! -f "$3" ]; then
+        echo "ERROR! Wrong path to ssl cert or key"
+        exit 1
+    fi
+
+    SSL_CERT="ssl_certificate_key $2;"
+    SSL_KEY="ssl_certificate_key $3;"
+fi
+
+if [ ! -d "$PATH_TO_WWW" ]
     echo "creating $PATH_TO_WWW directory"
     then mkdir -p $PATH_TO_WWW
 fi
@@ -28,7 +44,13 @@ cp ./maintenance /etc/init.d/maintenance
 cp ./000-maintenance /etc/nginx/sites-available/000-maintenance
 cp ./index.html $PATH_TO_WWW/index.html
 
-cat ./000-maintenance | sed -e "s/$SEARCH/$1/" > /etc/nginx/sites-available/000-maintenance
+cat ./000-maintenance | sed -e "s/{{servernames}}/$1/" > /tmp/maintenace.conf
+cat /tmp/maintenace.conf | sed -e "s/{{root}}/$PATH_TO_WWW/" > /tmp/maintenace.conf
+
+cat ./000-maintenance | sed -e "s/ssl_certificate;/$SSL_CERT/" > /tmp/maintenace.conf
+cat /tmp/maintenace.conf | sed -e "s/ssl_certificate_key;/$SSL_KEY/" > /tmp/maintenace.conf
+
+cp /tmp/maintenace.conf /etc/nginx/sites-available/000-maintenance
 echo "DONE!"
 echo ""
 echo "installed nginx-maintenance stat with /etc/init.d/maintenance start"
